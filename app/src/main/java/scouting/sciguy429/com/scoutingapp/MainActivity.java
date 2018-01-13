0b05849f43184b3de79123967384dfafc8061c9d
King <CKing254@fhsdschools.org>
2018-01-13 08:55:09 -0600

    Initial commit

---
+++ app/src/main/java/scouting/sciguy429/com/scoutingapp/MainActivity.java
@@ -0,0 +1,13 @@
+package scouting.sciguy429.com.scoutingapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
