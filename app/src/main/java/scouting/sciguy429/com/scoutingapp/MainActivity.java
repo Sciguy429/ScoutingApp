1b5db5bfe9e43ef92799702f53e0ea5ceff1f15b
Christopher KIng <sciguy429@gmail.com>
2018-02-02 19:44:11 -0600

    BT Test Code

--- app/src/main/java/scouting/sciguy429/com/scoutingapp/MainActivity.java
+++ app/src/main/java/scouting/sciguy429/com/scoutingapp/MainActivity.java
@@ -1,13 +1,78 @@
 package scouting.sciguy429.com.scoutingapp;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.Set;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
+
+    final String TAG = "ScoutingApp";
+    final String ServerBTMAC = "00:50:F2:7E:80:91";
+
+    BluetoothDevice ServerBTDevice = null;
+    BluetoothSocket mmSocket;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+            // Device doesn't support Bluetooth
+            Toast.makeText(this, "Device Dose Not Support Bluetooth", Toast.LENGTH_LONG).show();
+        }
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, 1);
+        }
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+
+        if (pairedDevices.size() > 0) {
+            // There are paired devices. Get the name and address of each paired device.
+            for (BluetoothDevice device : pairedDevices) {
+                String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress(); // MAC address
+                Log.d(TAG, deviceName + " : " + deviceHardwareAddress + " : " + deviceHardwareAddress.equals(ServerBTMAC));
+                if (deviceHardwareAddress.equals(ServerBTMAC)) {
+                    ServerBTDevice = device;
+                }
+            }
+        }
+        if (ServerBTDevice != null) {
+            try {
+                mmSocket = ServerBTDevice.createRfcommSocketToServiceRecord(UUID.fromString("05f71850-17bd-4805-a32d-d5e22be90d6d"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                // Connect to the remote device through the socket. This call blocks
+                // until it succeeds or throws an exception.
+                mmSocket.connect();
+            } catch (IOException connectException) {
+                // Unable to connect; close the socket and return.
+                try {
+                    mmSocket.close();
+                } catch (IOException closeException) {
+                    Log.e(TAG, "Could not close the client socket", closeException);
+                }
+            }
+            try {
+                for (int i = 0; i <= 100; i++) {
+                    mmSocket.getOutputStream().write(i);
+                }
+                //mmSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
