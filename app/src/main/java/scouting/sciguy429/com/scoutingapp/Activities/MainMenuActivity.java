c35af68c407862f5496b8f58b763837aa810b9e5
Christopher KIng <sciguy429@gmail.com>
2018-02-19 14:21:16 -0600

    Matches activity now opens

--- app/src/main/java/scouting/sciguy429/com/scoutingapp/Activities/MainMenuActivity.java
+++ app/src/main/java/scouting/sciguy429/com/scoutingapp/Activities/MainMenuActivity.java
@@ -19,6 +19,7 @@ public class MainMenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
 
+        matchesButton = findViewById(R.id.matchesButton);
         matchesButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -26,6 +27,7 @@ public class MainMenuActivity extends AppCompatActivity {
             }
         });
 
+        teamsButton = findViewById(R.id.teamsButton);
         teamsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -33,6 +35,7 @@ public class MainMenuActivity extends AppCompatActivity {
             }
         });
 
+        settingsButton = findViewById(R.id.settingsButton);
         settingsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
