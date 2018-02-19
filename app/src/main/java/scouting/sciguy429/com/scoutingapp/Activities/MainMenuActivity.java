67dc9cc846647a4450c6a8de5416d2258b3d1011
Christopher KIng <sciguy429@gmail.com>
2018-02-19 14:17:57 -0600

    Matches activity now opens

--- app/src/main/java/scouting/sciguy429/com/scoutingapp/Activities/MainMenuActivity.java
+++ app/src/main/java/scouting/sciguy429/com/scoutingapp/Activities/MainMenuActivity.java
@@ -1,15 +1,43 @@
 package scouting.sciguy429.com.scoutingapp.Activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import scouting.sciguy429.com.scoutingapp.R;
 
 public class MainMenuActivity extends AppCompatActivity {
 
+    Button matchesButton;
+    Button teamsButton;
+    Button settingsButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        matchesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), MatchesActivity.class));
+            }
+        });
+
+        teamsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
