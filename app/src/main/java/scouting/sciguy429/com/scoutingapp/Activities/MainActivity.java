4a6eebd028285af3376e19d8a2265d41469d5310
Christopher KIng <sciguy429@gmail.com>
2018-02-19 13:59:49 -0600

    Removed unneeded cast

--- app/src/main/java/scouting/sciguy429/com/scoutingapp/Activities/MainActivity.java
+++ app/src/main/java/scouting/sciguy429/com/scoutingapp/Activities/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        loginButton = (Button) findViewById(R.id.loginButton);
+        loginButton = findViewById(R.id.loginButton);
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
