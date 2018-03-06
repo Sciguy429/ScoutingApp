620c0bebc0df7a4e1888b301d8d6a04e7690d5e0
Christopher King <sciguy429@gmail.com>
2018-03-05 21:43:13 -0600

    test code

---
+++ app/src/main/java/scouting/sciguy429/com/scoutingapp/Activities/TeamViewActivity.java
@@ -0,0 +1,15 @@
+package scouting.sciguy429.com.scoutingapp.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import scouting.sciguy429.com.scoutingapp.R;
+
+public class TeamViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_team_view);
+    }
+}
---
+++ app/src/main/java/scouting/sciguy429/com/scoutingapp/Objects/MatchData.java
@@ -0,0 +1,61 @@
+package scouting.sciguy429.com.scoutingapp.Objects;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class MatchData implements Parcelable {
+    public int matchNumber;
+    public int teamR1;
+    public int teamR2;
+    public int teamR3;
+    public int teamB1;
+    public int teamB2;
+    public int teamB3;
+    public MatchData(int matchNumber, int teamR1, int teamR2, int teamR3, int teamB1, int teamB2, int teamB3) {
+        this.matchNumber = matchNumber;
+        this.teamR1 = teamR1;
+        this.teamR2 = teamR2;
+        this.teamR3 = teamR3;
+        this.teamB1 = teamB1;
+        this.teamB2 = teamB2;
+        this.teamB3 = teamB3;
+    }
+
+    private MatchData(Parcel in) {
+        matchNumber = in.readInt();
+        teamR1 = in.readInt();
+        teamR2 = in.readInt();
+        teamR3 = in.readInt();
+        teamB1 = in.readInt();
+        teamB2 = in.readInt();
+        teamB3 = in.readInt();
+    }
+
+    public static final Creator<MatchData> CREATOR = new Creator<MatchData>() {
+        @Override
+        public MatchData createFromParcel(Parcel in) {
+            return new MatchData(in);
+        }
+
+        @Override
+        public MatchData[] newArray(int size) {
+            return new MatchData[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeInt(matchNumber);
+        parcel.writeInt(teamR1);
+        parcel.writeInt(teamR2);
+        parcel.writeInt(teamR3);
+        parcel.writeInt(teamB1);
+        parcel.writeInt(teamB2);
+        parcel.writeInt(teamB3);
+    }
+}
