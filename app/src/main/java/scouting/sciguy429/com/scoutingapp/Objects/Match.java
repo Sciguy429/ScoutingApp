37cf82d24cc645748bdd4701b6a0e1485e3f5af1
Christopher KIng <sciguy429@gmail.com>
2018-03-06 16:35:01 -0600

    Removed parcelable implementation

--- app/src/main/java/scouting/sciguy429/com/scoutingapp/Objects/Match.java
+++ app/src/main/java/scouting/sciguy429/com/scoutingapp/Objects/Match.java
@@ -1,9 +1,6 @@
 package scouting.sciguy429.com.scoutingapp.Objects;
 
-import android.os.Parcel;
-import android.os.Parcelable;
-
-public class Match implements Parcelable {
+public class Match {
     public int matchNumber;
     public int teamR1;
     public int teamR2;
@@ -19,43 +16,5 @@ public class Match implements Parcelable {
         this.teamB1 = teamB1;
         this.teamB2 = teamB2;
         this.teamB3 = teamB3;
-    }
-
-    private Match(Parcel in) {
-        matchNumber = in.readInt();
-        teamR1 = in.readInt();
-        teamR2 = in.readInt();
-        teamR3 = in.readInt();
-        teamB1 = in.readInt();
-        teamB2 = in.readInt();
-        teamB3 = in.readInt();
-    }
-
-    public static final Creator<Match> CREATOR = new Creator<Match>() {
-        @Override
-        public Match createFromParcel(Parcel in) {
-            return new Match(in);
-        }
-
-        @Override
-        public Match[] newArray(int size) {
-            return new Match[size];
-        }
-    };
-
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    @Override
-    public void writeToParcel(Parcel parcel, int i) {
-        parcel.writeInt(matchNumber);
-        parcel.writeInt(teamR1);
-        parcel.writeInt(teamR2);
-        parcel.writeInt(teamR3);
-        parcel.writeInt(teamB1);
-        parcel.writeInt(teamB2);
-        parcel.writeInt(teamB3);
     }
 }
