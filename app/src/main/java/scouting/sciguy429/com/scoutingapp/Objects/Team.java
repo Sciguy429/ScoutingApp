3c5cb2e527f19083c397a8a885e0578e55451b35
Christopher KIng <sciguy429@gmail.com>
2018-03-07 15:04:04 -0600

    Made team object

--- app/src/main/java/scouting/sciguy429/com/scoutingapp/Objects/Team.java
+++ app/src/main/java/scouting/sciguy429/com/scoutingapp/Objects/Team.java
@@ -1,20 +1,29 @@
 package scouting.sciguy429.com.scoutingapp.Objects;
 
 public class Team {
-    public int matchNumber;
-    public int teamR1;
-    public int teamR2;
-    public int teamR3;
-    public int teamB1;
-    public int teamB2;
-    public int teamB3;
-    public Team(int matchNumber, int teamR1, int teamR2, int teamR3, int teamB1, int teamB2, int teamB3) {
-        this.matchNumber = matchNumber;
-        this.teamR1 = teamR1;
-        this.teamR2 = teamR2;
-        this.teamR3 = teamR3;
-        this.teamB1 = teamB1;
-        this.teamB2 = teamB2;
-        this.teamB3 = teamB3;
+    public int teamNumber;
+    public String teamName;
+    public float robotHeight;
+    public boolean canClimb;
+    public boolean canPlaceOnSwitch;
+    public boolean canPlaceOnScale;
+    public boolean canPlaceInPortal;
+    public boolean canPlaceInAnyOrientation;
+    public String driveTrainInfo;
+    public String cubePlaceMethod;
+    public String comments;
+
+    public Team(int teamNumber, String teamName, float robotHeight, boolean canClimb, boolean canPlaceOnSwitch, boolean canPlaceOnScale, boolean canPlaceInPortal, boolean canPlaceInAnyOrientation, String driveTrainInfo, String cubePlaceMethod, String comments) {
+        this.teamNumber = teamNumber;
+        this.teamName = teamName;
+        this.robotHeight = robotHeight;
+        this.canClimb = canClimb;
+        this.canPlaceOnSwitch = canPlaceOnSwitch;
+        this.canPlaceOnScale = canPlaceOnScale;
+        this.canPlaceInPortal = canPlaceInPortal;
+        this.canPlaceInAnyOrientation = canPlaceInAnyOrientation;
+        this.driveTrainInfo = driveTrainInfo;
+        this.cubePlaceMethod = cubePlaceMethod;
+        this.comments = comments;
     }
 }
